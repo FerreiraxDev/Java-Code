f8dbabaac53bc91675e00c0741d39dc93ecd0604
Ferreir4xDev <caiojuan02@gmail.com>
2024-08-27 19:22:52 -0300

    Modificação do arquivo Main na pasta CadastroFaculdade

--- CadastroFaculdade/src/main/java/org/poo/projeto/Main.java
+++ CadastroFaculdade/src/main/java/org/poo/projeto/Main.java
@@ -1,7 +1,25 @@
 package org.poo.projeto;
+import org.poo.projeto.usuario.Identificacao;
+import org.poo.projeto.usuario.Pessoa;
+
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Identificacao i = new Identificacao(
+                "Professor",
+                "1100394");
+
+        Pessoa p = new Pessoa(
+                "Caio",
+                "0320320323",
+                2002,
+                i
+        );
+
+        System.out.println("Nome: " + p.getNome());
+        System.out.println("CPF: " + p.getCPF());
+        System.out.println("Ano de Nascimento: " + p.getAnoIdade());
+        System.out.println("Bem vindo ao sistema, " + p.getIdentificacao().getFuncao() + ", seu número de matrícula é: " + p.getIdentificacao().getMatricula());
+
     }
 }
